f206c44114fc7bd6660f1b320dd4f5f312108535
LiuQi0811 <1139642444@qq.com>
2022-12-13 17:27:33 +0800

    ElasticSearch 8 测试

--- src/test/java/com/code/takeaway/TestElasticsearch7.java
+++ src/test/java/com/code/takeaway/TestElasticsearch7.java
@@ -1,10 +1,13 @@
 package com.code.takeaway;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.cat.indices.IndicesRecord;
+import co.elastic.clients.elasticsearch.indices.IndexState;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.Transport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
+import com.alibaba.fastjson.JSON;
 import com.code.takeaway.pool.ElasticSearchClientPool;
 import com.code.takeaway.utils.CommonInterfaceMethodsEs8;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +19,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author LiuQi
@@ -45,9 +50,12 @@ public class TestElasticsearch7 {
     }
 
     @Test
-    public void testDelete() throws Exception {
+    public void test() throws Exception {
         CommonInterfaceMethodsEs8 commonInterfaceMethodsEs8 = new CommonInterfaceMethodsEs8();
-        commonInterfaceMethodsEs8.deleteIndex("user");
+        List<IndicesRecord> allIndex = commonInterfaceMethodsEs8.getAllIndex();
+        log.error(" 全部索引信息 {}", JSON.toJSON(allIndex));
+        Map<String, IndexState> user = commonInterfaceMethodsEs8.getIndexInfo("user");
+        log.error(" 根据索引名称 获取 索引信息 {}", JSON.toJSON(allIndex));
     }
 
 
